c60a21f7f64ee4215480598069ac2bbec7c73b55
Pinton26 <112706515+Pinton26@users.noreply.github.com>
2023-05-09 00:13:41 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -13,8 +13,8 @@ const USER_CONFIG = {
   // 使用微信测试号：公众号APP_SECRET
   APP_SECRET: '06c4348610b5beabf6645cdd8ae23a86',
 
-  PROVINCE: '广东',
-  CITY: '广州',
+  PROVINCE: '广东省',
+  CITY: '广州市',
 
   USERS: [
     {

5eaa63591603f79dfbd15e7589168e40ea21954d
Pinton26 <112706515+Pinton26@users.noreply.github.com>
2023-05-09 11:30:51 +0800

    Update index.cjs

--- config/index.cjs
+++ config/index.cjs
@@ -67,7 +67,7 @@ const USER_CONFIG = {
       // 使用微信测试号：扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
       id: 'ooxRR57wENvMeXmsyeTowPLNNT7o',
       // 使用微信测试号：你想对他发送的模板消息的模板ID
-      useTemplateId: '-WKKPcXTCzDXZPji6eFmve5EwA54OvwuAtis8EUc6oY',
+      useTemplateId: 'faq31gUX1T7Yms7-iMTNePvZ_OSWn9Rr3kx6K1WbRNo',
       // 新历生日, 仅用作获取星座运势, 格式必须为MM-DD
       horoscopeDate: '12-26',
       province: '广东省',
